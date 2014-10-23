beb7878a635b24ed94e704aa5bbfb4daca6b1fd9
Misoka <afagernes@bredband.net>
2014-10-23 17:18:54 +0200

    "Currency"- och "Customer"-klass.
    Lagt till "Currency"- och "Customer"-klasser. Börjat skriva klasser på
    engelska istället för svenska.

---
+++ Customer.java
@@ -0,0 +1,19 @@
+public class Customer {
+	private String name;
+	
+	//private Discount discount = new Discount();
+	
+	public Customer(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	/*
+	public Discount getDiscount(){
+		return discount;
+	}
+	*/
+}
