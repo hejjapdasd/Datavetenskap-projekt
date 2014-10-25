cf5cc0e4b56b42f796d427f4254de1e63767163e
N1fighter <johansallstrom7@gmail.com>
2014-10-25 16:31:10 +0200

    Update Kvitto.java
    -Har gjort om listan till HasMap
    -uppdaterat addVara()
    -uppdaterat toString()
    -lagt till metoderna removeVara() och summeraAllaVaror()

--- Kvitto.java
+++ Kvitto.java
@@ -1,23 +1,69 @@
 import java.util.*;
+import java.util.Map.Entry;
 
 
 public class Kvitto {
-	ArrayList<Vara> list = new ArrayList<Vara>();	
+	Map<vara, Integer> varulist = new HashMap<>();
+//	Integer står för antalet av varje vara
+		
 	
 	public Kvitto(){
 		
 		
 		}
-		
-	public void addVara(Vara v){
-		list.add(v);	
+	
+	public void addVara(vara v){
+		if(varulist.containsKey(v)){
+			Integer nyttAntal = varulist.get(v)+1;
+			vara newVara = v;
+			removeVara(v);
+//			ta bort det gamla värdet
+			varulist.put(newVara, nyttAntal);
+		}else{
+			varulist.put(v, 1);
+		}
 	}
+		
+	public void removeVara(vara v){
+		if(varulist.containsKey(v)){
+			varulist.remove(v);
+		}
+	}
+	
+	
+
+	public float summeraAllaVaror(){
+		float sum = 0;
+		for (vara v : varulist.keySet()) {
+			   sum += v.getPris()*varulist.get(v);
+//			   			^^^^^^fixa fram nyckeln dvs. antalet av varan
+			}
+		return sum;
+	}
+	
+	
+		
+//	public void addVara(vara v){
+//		for(int i;i< varulist.size(); i++){
+//			if(i.equals(v)){
+//				Integer nyttAntal = varulist.get(i)+1;
+//				varulist.remove(i);
+////				ta bort det gamla värdet
+//				varulist.put(v, nyttAntal);
+//			}else{
+//				varulist.put(v, 1);
+//			}
+//		}
+//		
+//		
+//	}
 	
 	public String toString(){
 		String s = "";
-		for(Vara v: list){
+		for (vara v : varulist.keySet()) {
 			s += v.toString();
-		}
+//			   						
+			}
 		return s;
 	}
 	
